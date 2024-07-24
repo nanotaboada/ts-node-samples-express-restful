06d68c24fe10d6003bb3ffd16ec3e883755fdeb5
Nano Taboada <87288+nanotaboada@users.noreply.github.com>
2024-07-24 11:17:28 -0300

    feat: add Swagger endpoint with API docs

--- src/controllers/player-controller.ts
+++ src/controllers/player-controller.ts
@@ -6,11 +6,56 @@ import { Request, Response } from 'express';
 import playerService from '../services/player-service';
 import { Player } from '../models/player-model';
 
+/**
+ * @swagger
+ * tags:
+ *   name: Players
+ */
+
 const playerController = {
+    /**
+     * @swagger
+     * /players:
+     *   get:
+     *     summary: Retrieves all players
+     *     tags: [Players]
+     *     responses:
+     *       200:
+     *         description: OK
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: array
+     *               items:
+     *                 $ref: '#/components/schemas/Player'
+     */
     getAll: (request: Request, response: Response): void => {
         const players = playerService.retrieveAll();
         response.json(players);
     },
+    /**
+     * @swagger
+     * /players/{id}:
+     *   get:
+     *     summary: Retrieves a Player by its ID
+     *     tags: [Players]
+     *     parameters:
+     *       - in: path
+     *         name: id
+     *         schema:
+     *           type: integer
+     *         required: true
+     *         description: Player.id
+     *     responses:
+     *       200:
+     *         description: OK
+     *         content:
+     *           application/json:
+     *             schema:
+     *               $ref: '#/components/schemas/Player'
+     *       404:
+     *         description: Not Found
+     */
     getById: (request: Request, response: Response): void => {
         const id = parseInt(request.params.id);
         const player = playerService.retrieveById(id);
@@ -20,6 +65,29 @@ const playerController = {
             response.sendStatus(404);
         }
     },
+    /**
+     * @swagger
+     * /players/squadNumber/{squadNumber}:
+     *   get:
+     *     summary: Retrieves a Player by its Squad Number
+     *     tags: [Players]
+     *     parameters:
+     *       - in: path
+     *         name: squadNumber
+     *         schema:
+     *           type: integer
+     *         required: true
+     *         description: Player.squadNumber
+     *     responses:
+     *       200:
+     *         description: OK
+     *         content:
+     *           application/json:
+     *             schema:
+     *               $ref: '#/components/schemas/Player'
+     *       404:
+     *         description: Not Found
+     */
     getBySquadNumber: (request: Request, response: Response): void => {
         const squadNumber = parseInt(request.params.squadNumber);
         const player = playerService.retrieveBySquadNumber(squadNumber);
@@ -29,6 +97,26 @@ const playerController = {
             response.sendStatus(404);
         }
     },
+    /**
+     * @swagger
+     * /players:
+     *   post:
+     *     summary: Creates a new Player
+     *     tags: [Players]
+     *     requestBody:
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             $ref: '#/components/schemas/Player'
+     *     responses:
+     *       201:
+     *         description: Created
+     *       400:
+     *         description: Bad Request
+     *       409:
+     *         description: Conflict
+     */
     post: (request: Request, response: Response): void => {
         const id = parseInt(request.body.id);
         const player: Player = request.body;
@@ -43,6 +131,33 @@ const playerController = {
             response.sendStatus(400);
         }
     },
+    /**
+     * @swagger
+     * /players/{id}:
+     *   put:
+     *     summary: Updates (entirely) a Player by its ID
+     *     tags: [Players]
+     *     parameters:
+     *       - in: path
+     *         name: id
+     *         schema:
+     *           type: integer
+     *         required: true
+     *         description: Player.id
+     *     requestBody:
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             $ref: '#/components/schemas/Player'
+     *     responses:
+     *       204:
+     *         description: No Content
+     *       400:
+     *         description: Bad Request
+     *       404:
+     *         description: Not Found
+     */
     put: (request: Request, response: Response): void => {
         const id = parseInt(request.params.id);
         const player: Player = request.body;
@@ -57,6 +172,25 @@ const playerController = {
             response.sendStatus(400);
         }
     },
+    /**
+     * @swagger
+     * /players/{id}:
+     *   delete:
+     *     summary: Deletes a Player by its ID
+     *     tags: [Players]
+     *     parameters:
+     *       - in: path
+     *         name: id
+     *         schema:
+     *           type: integer
+     *         required: true
+     *         description: Player.id
+     *     responses:
+     *       204:
+     *         description: No Content
+     *       404:
+     *         description: Not Found
+     */
     delete: (request: Request, response: Response): void => {
         const id = parseInt(request.params.id);
         if (!playerService.retrieveById(id)) {
