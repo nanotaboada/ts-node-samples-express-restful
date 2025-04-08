a6d5a616392bf4d2ab5bdabfffa3d5725fc61010
Nano Taboada <87288+nanotaboada@users.noreply.github.com>
2025-04-08 18:59:56 -0300

    chore(test): adjust Jest coverage report scope

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,7 @@ const config: Config = {
     verbose: true,
     preset: 'ts-jest',
     testEnvironment: 'node',
+    rootDir: '.', // ensures all paths are relative from project root
     testMatch: ['<rootDir>/tests/**/*-test.ts'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     transform: {
@@ -16,7 +17,27 @@ const config: Config = {
     },
     moduleDirectories: ['node_modules', 'src'],
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-    coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
+
+    // Only include coverage for controllers, services, and routes
+    collectCoverage: true,
+    collectCoverageFrom: ['src/controllers/**/*.{ts,tsx}', 'src/services/**/*.{ts,tsx}', 'src/routes/**/*.{ts,tsx}'],
+
+    // Output clean relative paths
+    coverageReporters: ['text', 'json', 'clover', 'lcov'],
+    coverageDirectory: 'coverage',
+
+    // Additional ignore patterns
+    coveragePathIgnorePatterns: [
+        '/node_modules/',
+        '/dist/',
+        '<rootDir>/src/data/',
+        '<rootDir>/src/docs/',
+        '<rootDir>/src/middlewares/',
+        '<rootDir>/src/models/',
+        '<rootDir>/src/app.ts',
+        '<rootDir>/src/server.ts',
+        '<rootDir>/tests/',
+    ],
 };
 
 export default config;
